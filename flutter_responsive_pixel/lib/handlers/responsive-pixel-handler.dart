08240df6ee0a22b07e6712692f060fb9d4cdaf77
Nagai <Rafael.Nagai@btgpactual.com>
2020-01-29 17:04:06 -0300

    Corrigido o problema de não deixar responsivel corretamente, simplificado o codigo do handler.

--- flutter_responsive_pixel/lib/handlers/responsive-pixel-handler.dart
+++ flutter_responsive_pixel/lib/handlers/responsive-pixel-handler.dart
@@ -1,33 +1,16 @@
-import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 
 class ResponsivePixelHandler{
-  static double _basePixel;
-  static double responsivePixel;
+  static double _baseWidth;
 
   static void init({
-    double basePixel = 8,
-    double baseWidth = 1125,
-    double minResponsivePixel = 6,
-    double maxResponsivePixel = 12,
-    double minWidth = 700,
-    double maxWidth = 1440
+    double baseWidth = 1440,
   }){
-    _basePixel = basePixel;
-
-    double widthMobile = window.physicalSize.width;
-    widthMobile = widthMobile < minWidth ? minWidth : widthMobile;
-    widthMobile = widthMobile > maxWidth ? maxWidth : widthMobile;
-
-    responsivePixel = widthMobile * basePixel / baseWidth;
-
-    responsivePixel = responsivePixel < minResponsivePixel ? minResponsivePixel : responsivePixel;
-    responsivePixel = responsivePixel > maxResponsivePixel ? maxResponsivePixel : responsivePixel;
+    _baseWidth = baseWidth;
   }
 
-  static double toResponsivePixel(double pixel, {double minResponsivePixel, double maxResponsivePixel}){
-    double proportionPixel = pixel / _basePixel;
-
-    double valuePixel = responsivePixel * proportionPixel;
+  static double toResponsivePixel(double pixel, BuildContext context, {double minResponsivePixel, double maxResponsivePixel}){
+    double valuePixel = (pixel / _baseWidth) * MediaQuery.of(context).size.width;
     if(minResponsivePixel != null)
       valuePixel = valuePixel < minResponsivePixel ? minResponsivePixel : valuePixel;
     if(maxResponsivePixel != null)
--- flutter_responsive_pixel/lib/main.dart
+++ flutter_responsive_pixel/lib/main.dart
@@ -1,56 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_pixel/handlers/responsive-pixel-handler.dart';
 
+import 'handlers/responsive-pixel-handler.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ResponsivePixelHandler.init();
+    ResponsivePixelHandler.init(
+      baseWidth: 480,
+    );
     return MaterialApp(
       title: 'Flutter Responsive Pixel',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.width);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        color: Colors.blue,
-        width: ResponsivePixelHandler.toResponsivePixel(200),
-        padding: EdgeInsets.all(ResponsivePixelHandler.toResponsivePixel(50)),
-        margin: EdgeInsets.all(ResponsivePixelHandler.toResponsivePixel(50)),
-        child: Center(child: Text("Box")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            color: Colors.amber,
+            width: ResponsivePixelHandler.toResponsivePixel(240, context),
+            child: Text("Box", 
+              textAlign: TextAlign.center, 
+              style: TextStyle(fontSize: ResponsivePixelHandler.toResponsivePixel(16, context)),),
+          ),
+          Container(
+            color: Colors.blueGrey,
+            width: ResponsivePixelHandler.toResponsivePixel(240, context),
+            margin: EdgeInsets.only(left: ResponsivePixelHandler.toResponsivePixel(240, context)),
+            child: Text("Box", 
+              textAlign: TextAlign.center, 
+              style: TextStyle(fontSize: ResponsivePixelHandler.toResponsivePixel(36, context)),),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
